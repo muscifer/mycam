2a61ed245d69c6519d90db3a05bce1085f6135fb
muscifer <muscifer@gmail.com>
2013-06-02 14:25:43 +0800

    fistcommit

---
+++ mycam/mycam/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mycam
+//
+//  Created by 杨 曦 on 13-6-2.
+//  Copyright (c) 2013年 杨 曦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
